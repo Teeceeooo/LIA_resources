ae0464e290c693b3411a1084165d96af1dafa782
Sebastian Granlund <sebastian.granlund@live.se>
2023-05-15 14:45:49 +0200

    ändingar

---
+++ Allt.sql
@@ -0,0 +1,257 @@
+CREATE DATABASE liadb
+    DEFAULT CHARACTER SET utf8mb4
+    DEFAULT COLLATE utf8mb4_unicode_ci;
+
+USE liadb;
+
+
+
+	
+CREATE TABLE Item ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(255) NOT NULL, 
+icon VARCHAR(255), 
+description VARCHAR(1000) NOT NULL 
+); 
+
+CREATE TABLE Origin ( 
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(99) NOT NULL, 
+description VARCHAR(1000) NOT NULL 
+); 
+
+CREATE TABLE Trait ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(99) NOT NULL, 
+description VARCHAR(1000) NOT NULL 
+); 
+
+CREATE TABLE Champion ( 
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(255) NOT NULL, 
+ultimateInfo VARCHAR(255) NOT NULL, 
+cost INT NOT NULL, 
+recommendedItemIds VARCHAR(255), 
+recommendedOriginIds VARCHAR(255), 
+recommendedTraitIds VARCHAR(255)
+);
+
+CREATE TABLE User (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(255) NOT NULL UNIQUE,
+password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO User (username, password) VALUES ('admin', 'root');
+
+INSERT INTO Champion (name, ultimateInfo, cost, recommendedItemIds, recommendedOriginIds, recommendedTraitIds) 
+VALUES 
+  ('Ahri', 'Spirit Rush - Ahri dashes around the battlefield, damaging enemies she passes through.', 2,  '1,3,5', '1', '2,5'),
+  ('Darius', 'Dreadnova Guillotine - Darius leaps to the farthest enemy, dealing damage and executing targets below a threshold.', 3,  '2,4', '2', '1,3'),
+  ('Ezreal', 'Trueshot Barrage - Ezreal channels for a few seconds, firing a barrage of missiles towards the enemy with the highest current health.', 4,  '3,4,5', '1', '4'),
+  ('Katarina', 'Death Lotus - Katarina spins and throws daggers at the nearest enemies, dealing damage and reducing healing.', 5,  '1,2,3,4,5', '2', '1');
+
+INSERT INTO Item (name, description) VALUES ('DeathBlade', 'Grant 66% bonus Attack Damage.');
+INSERT INTO Item (name, description) VALUES ('Giantslayer', 'Abilities and attacks deal 25% more damage to enemies with more than 1600 maximum Health.');
+INSERT INTO Item (name, description) VALUES ('Edge of Night', 'Once per combat: At 60% Health, briefly become stealthed, becoming untargetable and shedding negative effects. Then, grant 15% bonus Attack Speed.');
+INSERT INTO Item (name, description) VALUES ('Bloodthirster', 'Grant 20% Omnivamp. Once per combat: At 40% Health, gain a 25% maximum Health shield that lasts up to 5 seconds.');
+INSERT INTO Item (name, description) VALUES ('Hextech Gunblade', 'Grant 22% Omnivamp. Damage heals the lowest percent Health ally for the same amount.');
+INSERT INTO Item (name, description) VALUES ('Spear of Shojin', 'Every 3rd attacks restores 20 additional Mana.');
+INSERT INTO Item (name, description) VALUES ('Zekes Harald', 'Combat start: Grant 20% Attack Speed to the holder and allies within 1 hex in the same row.');
+INSERT INTO Item (name, description) VALUES ('Infinity Edge', 'Damage from an Ability can critically strike.');
+INSERT INTO Item (name, description) VALUES ('InfiniTeam Emblem', 'The holder gains the InfiniTeam trait.');
+INSERT INTO Item (name, description) VALUES ('Rapid Firecannon', 'Grant 2 bonus Attack Range and your attacks cannot miss.');
+INSERT INTO Item (name, description) VALUES ('Titans Resolve', 'Grant 2% Attack Damage and 2 Ability Power when attacking or taking damage, stacking up to 25 times.');
+INSERT INTO Item (name, description) VALUES ('Runaans Hurricane', 'Attacks fire a bolt at a nearby enemy, dealing 50% Attack Damage as physical damage.');
+INSERT INTO Item (name, description) VALUES ('Guinsoos Rageblade', 'Attacks grant 5% bonus Attack Speed. This effect stacks.');
+INSERT INTO Item (name, description) VALUES ('Statikk Shiv', 'Every 3rd attack unleashes chain lightning that bounces to 4 enemies, dealing 30 magic damage and Shredding them for 5 seconds.');
+INSERT INTO Item (name, description) VALUES ('ZZrot Portal', 'Combat start: Taunt. \nOn death, a Voidspawn arises, Taunting nearby enemies. That summoned units will not have as much effectiveness. \nTaunt: enemies that are able and in range must attack the taunter');
+INSERT INTO Item (name, description) VALUES ('Last Whisper', 'Any physical damage dealt Sunder the target for 5 seconds. This effect does not stack. \n[Unique - Only One Per Champion] \nSunder: reduce Armor by 50%');
+INSERT INTO Item (name, description) VALUES ('Duelist Emblem', 'The holder gains the Duelist trait.');
+INSERT INTO Item (name, description) VALUES ('Bramble Vest', 'Negates 75% bonus damage from incoming critical strikes. When struck by an attack, deal 75/100/150 magic damage to all nearby enemies (once every 2.5 seconds).');
+INSERT INTO Item (name, description) VALUES ('Gargoyle Stoneplate', 'Grant 15 Armor and 15 Magic Resist for each enemy targeting the holder.');
+INSERT INTO Item (name, description) VALUES ('Locket of the Iron Solari', 'Combat start: Shield the holder and allies within 2 hexes in the same row for 300/350/400 damage for 15 seconds.');
+INSERT INTO Item (name, description) VALUES ('Protectors Vow', 'Once per combat: At 40% Health, gain a 25% maximum Health shield that lasts up to 5 seconds and 35 Armor and Magic Resist for the rest of combat.');
+INSERT INTO Item (name, description) VALUES ('Sunfire Cape', 'Every 2 seconds, an enemy within 2 hexes is 10% Burned and 33% Wounded for 10 seconds.');
+INSERT INTO Item (name, description) VALUES ('Shroud of Stillness', 'Combat start: Shoots a beam that 35% Mana Reaves enemies.');
+INSERT INTO Item (name, description) VALUES ('Oxforce Emblem', 'The holder gains the Ox Force trait.');
+INSERT INTO Item (name, description) VALUES ('Dragons Claw', 'Every 2 seconds, regenerate 4% maximum Health.');
+INSERT INTO Item (name, description) VALUES ('Ionic Spark', '50% Shred enemies within 2 hexes. When enemies cast an Ability, they are also zapped for magic damage equal to 185% of their maximum Mana. \n[Direct damage item] \nShred: reduce Magic Resist ');
+INSERT INTO Item (name, description) VALUES ('Chalic of Power', 'Combat start: Grant 25 Ability Power to the holder and allies within 1 hex in the same row.');
+INSERT INTO Item (name, description) VALUES ('Zeptyr', 'Combat start: Summon a whirlwind on the opposite side of the arena that removes the closest enemy from combat for 5 seconds.');
+INSERT INTO Item (name, description) VALUES ('Quicksilver', 'Combat start: Grant immunity to crowd control for 15 seconds.');
+INSERT INTO Item (name, description) VALUES ('Deathcap', 'Grants 70 bonus Ability Power');
+INSERT INTO Item (name, description) VALUES ('Archangels Staff', 'Combat start: Grant 20 Ability Power every 5 seconds.');
+INSERT INTO Item (name, description) VALUES ('Morellonomicon', 'Magic or true damage from an Ability 10% Burns and 33% Wounds enemies for the 10 seconds.');
+INSERT INTO Item (name, description) VALUES ('Jeweled Gauntlet', 'Grant 15 bonus Ability Power and 15% Critical Strike Chance. \n \nDamage from an Ability can critically strike.');
+INSERT INTO Item (name, description) VALUES ('Anima Squad Emblem', 'The holder gains the Anima Squad trait.');
+INSERT INTO Item (name, description) VALUES ('Blue Buff', 'Abilities cost 10 less Mana to cast. \n If the holder gets at least 1 takedown within 3 seconds of casting, gain 10.');
+INSERT INTO Item (name, description) VALUES ('Redemption', 'Heal allies within 1 hex for 15% of their missing Health every 5 seconds. They also take 25% less multi-target damage for 5 seconds (damage reduction does not stack).');
+INSERT INTO Item (name, description) VALUES ('Hand of Justice', 'Grant 2 effects: \n - +15 Attack Damage and +15 Ability Power. \n - 15% Omnivamp. \n \n Each round, randomly double 1 of these effects. \n Omnivamp: heal for a percentage of damage dealt ');
+INSERT INTO Item (name, description) VALUES ('Heart Emblem', 'The holder gains the Heart trait.');
+INSERT INTO Item (name, description) VALUES ('Warmogs Armor', 'Grant 800 bonus Health.');
+INSERT INTO Item (name, description) VALUES ('Guardbreaker', 'After damaging a shielded enemy, Abilities and attacks deal 30% more damage for 3 seconds.');
+INSERT INTO Item (name, description)  VALUES ('Mascot Emblem', 'The holder gains the Mascot trait.');
+INSERT INTO Item (name, description)  VALUES ('Thieves Gloves', 'Each round: Equip 2 random items. \n [Consumes 3 item slots.]');
+INSERT INTO Item (name, description)  VALUES ('Renegade Emblem', 'The holder gains the Renegade trait.');
+INSERT INTO Item (name, description)  VALUES ('Tacticians Crown', 'Your team gains +1 maximum team size.');
+
+INSERT INTO Origin (name, description)
+VALUES ('A.D.M.I.N.', 'A.D.M.I.N. programs a custom configuration per player each game.');
+INSERT INTO Origin (name, description)
+VALUES ('Anima Squad', 'Anima Squad members pose after champion kills, increasing their fame. Each point of fame grants 5 permanent Health to each Anima Squad member. They also gain:
+(3) 10% Attack Damage and 10 Ability Power
+(5) 35% Attack Damage and 35 Ability Power
+(7) 60% Attack Damage and 60 Ability Power');
+INSERT INTO Origin (name, description)
+VALUES ('Gadgeteen', 'Each round, Gadgeteens create random modified weapons with powerful effects that fall apart after one round. Gadgeteens also gain Damage and Damage Reduction for each item equipped to them.
+(3) Create 1 item, 3% per item.
+(5) Create 2 item, 10% per item.');
+INSERT INTO Origin (name, description)
+VALUES ('InfiniTeam', 'The InfiniTeam opens a portal to an alternate timeline. At the start of combat, any InfiniTeam unit placed on a portal summons an alternate version of themselves with different items. They get 1 for each Augment you own.
+The copies have reduced Health and damage.
+(3) 1 portal opens, 50% damage and 50% HP.
+(5) 2 portals open, 55% damage and 55% HP.
+(7) 2 portals open, 85% damage and 85% HP.');
+INSERT INTO Origin (name, description)
+VALUES ('LaserCorps', 'When a LaserCorp agent attacks or is hit by an attack, their combat drone deals magic damage to the agents target (0.4 second cooldown). When a LaserCorps agent dies, their drone is reassigned to the nearest living agent.
+Drones lasers deal:
+(3) 27 magic damage.
+(4) 35 magic damage. All units get a LaserCorp drone.
+(5) 55 magic damage. All units get a LaserCorp drone.
+(6) 80 magic damage. All units get a LaserCorp drone.');
+INSERT INTO Origin (name, description)
+VALUES ('Mecha: PRIME', 'Use the Mecha selector item to choose a PRIME.
+Combat start: the PRIME combines with the 2 nearest Mecha, absorbing 90% of their Health.
+(3) The PRIME gains 60 Ability Power and 60% Attack Damage
+(5) All Mechas gain 55 Ability Power and 55% Attack Damage; The PRIME gains double this amount.');
+INSERT INTO Origin (name, description)
+VALUES ('Ox Force', 'Ox Force units gain bonus defenses. Once per combat, when they would drop below one HP, they instead shield themselves for 50% of their maximum health for 1.5 seconds.
+(2) 5 Armor and Magic Resist
+(4) 35 Armor and Magic Resist
+(6) 75 Armor and Magic Resist');
+INSERT INTO Origin (name, description)
+VALUES ('Riftwalker', 'Riftwalkers open a gap between dimensions and summon their ally Zac, who grows in power based on the star level of Riftwalkers.
+Zac gains the last-listed Trait of the closest Riftwalker.
+(3) Summon Zac');
+INSERT INTO Origin (name, description)
+VALUES ('Star Guardian', 'Gain more Mana from all sources.
+(2) 35% additional mana
+(4) 70% additional mana
+(6) 120% additional mana
+(8) 175% additional mana');
+INSERT INTO Origin (name, description)
+VALUES ('Supers', '(3) Combat start: strike a pose that grants your team 18% bonus damage, which increases by 3% for every 3-star champion on your team.');
+INSERT INTO Origin (name, description)
+VALUES ('Threat', '(1) Threats do not have a Trait bonus, but instead have powerful Abilities and increased base stats.');
+INSERT INTO Origin (name, description)
+VALUES ('Underground', 'The Underground must sneak through the sewers and crack 10 locks to open a vault. When the vault opens, you may choose to take the loot now, or attempt another heist for even better rewards.
+(3) Crack 2 vault lock after a player combat win, and 3 after a player combat loss
+(4) Crack 2 vault lock after a player combat win, and 4 after a player combat loss
+(5) Crack 3 vault lock after a player combat win, and 5 after a player combat loss
+(6) Crack 4 vault lock after a player combat win, and 7 after a player combat loss');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Ace', 'This trait is active only when you have exactly 1 or 4 unique Aces.\n
+(1) Execute enemies under 15% Health\n
+(4) Execute enemies under 30% Health');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Aegis', 'Your team gains bonus Magic Reseist, and Aegis units gain more.\n
+(2) +20 Magic Resist, +40 for Aegis \n
+(3) +40 Magic Resist, +80 for Aegis\n4 +60 Magic Resist, +120 for Aegis\n
+(5) +90 Magic Resist, +180 for Aegis');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Brawler', 'Brawlers gain additional maximum Health. \n
+(2) +20% maximum Health.\n
+(4) +45% maximum Health.\n
+(6) +70% maximum Health.\n
+(8) +100% maximum Health');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Corrupted', '(1) Combat starts: Lie dormant while absorbing the souls of allies that die. Gain 40 Ability Power for each Soul.
+\nOnce per combat at 70% Health (or when your team has died), come alive and fight.) ');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Defender', 'Your team gains bonus Armor, and Defender gain more. \n
+(2) +25 Armor, +50 for Defenders. \n
+(4)+75 Armor, +150 for Defenders. \n
+(6)+200 Armor, +400 for Defenders. ');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Duelist', 'Innate: Increased movement speed.\n
+Duelists basic attacks grant bonus Attack Speed, up to 12 stacks.\n
+(2) +5% Attack Speed per stack.\n
+(4) +8% Attack Speed per stack.\n
+(6) +10% Attack Speed per stack, Duelist take 10% less damage. \n
+(8) +16% Attack Speed per stack, Duelist take 20% less damage');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Forecaster', 'At the start of combat, Forecaster champions grant adjacent allies different buffs depending on the current weather, which changes every game.\n
+Sunny Weather:\n
+Combat start: Grant a 400/600/4000 Health shield to adjacent allies for 10/10/45 seconds.\n
+Windy Weather:\n
+Combat start: grant bonus Attack Damage and Ability Power to adjacent allies, increasing by 15/25/150% every 5 seconds.\n
+Rainy Weather:\n
+At the start of combat, Forecaster champions grant adjacent allies 20 / 40 / 100 mana until they have cast 1/1/100 spells.');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Hacker', ' Hackers gain Omnivamp and summon a H4ckerr!m. 
+Any unit placed in the rider hex will be sent to the enemy backline and is untargetable for the first 2 seconds of combat.\n
+Omnivamp: heal for a percentage of damage dealt.\n
+(3) +10% Omnivamp for Hackers.\n
+(4) +20% Omnivamp for Hackers, +15% for the rider.\n
+(5) +40% Omnivamp for Hackers, +30% for the rider.');
+
+
+INSERT INTO Trait (name, description) 
+VALUES ('Heart', 'When Heart units cast their Ability, your team gains stacking Ability Power for the rest of combat.\n
+(2) +4 Ability Power\n
+(4) +7 Ability Power\n
+(6) +11 Ability Power');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Mascot', 'Your team heals a percentage of their maximum Health every 2 seconds, and Mascots heal double the amount.\n
+When Mascots die, they retreat to the sidelines to cheer on your team. Your teams healing increased by 1% for each cheering Mascot.\n
+(2) 1.5% healing\n
+(4) 3% healing\n
+(6) 6% healing');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Parallel', '(2) Ezreal learns from his older, more handsome self and joins him in future adventures.\n
+Ezreals Ability becomes a blast that hits all enemies in a line.\n
+Ultimate Ezreals Ability summons two additional temporal duplicates.');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Prankster', '(2) Once per combat at 50% Health, spawn a target dummy, move to a safe location, and restore 150 Health.\n
+(3) And, Prankster dummies stun the enemy that killed them for 1.5 seconds.\n
+(4) The Health restore is increased to 350 Health and the stun duration become 2 second.');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Quickdraw', 'After every 2 damaging ability projectiles, Quickdraw units fire a bonus ability projectile. Bonus shots deal: \n
+(2) 70% total damage.\n
+(3) 140% total damage.\n
+(4) 220% total damage.');
+
+
+INSERT INTO Trait (name, description) 
+VALUES ('Renegade', 'Renegade units deal bonus damage, and the last sone standing deals more.\n
+(3) 25% bonus damage, 20% for last alive\n
+(5) 40% bonus damage, 30% for last alive\n
+(7) 70% bonus damage, 40% for last alive');
+
+
+INSERT INTO Trait (name, description) 
+VALUES ('Spellslinger', 'Every 5 seconds, the next attack instead fires a magic orb at a random target, which explodes for 50% Ability Power as magic damage.\n
+(2) +25 Ability Power\n
+(4) +45 Ability Power\n
+(6) +70 Ability Power\n
+(8) +110 Ability Power, Orb Damage 100%
+');
+
+INSERT INTO Trait (name, description) 
+VALUES ('Sureshot', 'Combat start: gain bonus Attack Damage now, and every 4 seconds.\n
+(2) +7% Attack Damage\n
+(3) +12% Attack Damage\n
+(4) +18% Attack Damage\n
+(5) +25% Attack Damage');
+
